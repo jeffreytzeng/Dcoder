f589c0a2c24412f05a76a07d0241137afb1e72a8
jeffreytzeng <2013gi20@gmail.com>
2019-11-22 15:33:19 +0800

    Adding easy level case 13.

---
+++ easy/13. Learn Sum of numbers/SumNumbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class SumNumbers {
+	public static void main(String[] arguments) {
+		Scanner input = new Scanner(System.in);
+		int num1 = input.nextInt();
+		int num2 = input.nextInt();
+		System.out.print(num1 + num2);
+	}
+}
